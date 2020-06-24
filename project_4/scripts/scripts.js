f04dbcc1081c5737109944e2df569501ae8a93c6
Cooper Goeke <cgoeke@redventures.com>
2020-06-23 21:14:24 -0400

    puzzle

--- project_4/scripts/scripts.js
+++ project_4/scripts/scripts.js
@@ -1,6 +1,14 @@
-// First Function
+// What is sent to the console?
 
-function sum (a, b) {
-  let c = a + b;
-  return c;
+function foobar (value1) {
+  let value2 = value1 / value1
+  return value2;
 }
+
+let x = 0;
+
+x = 2;
+
+let y = foobar(x + 1);
+
+//console.log(y)
