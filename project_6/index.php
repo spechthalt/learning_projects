af1e4bb4e1b4bebe33c4b8fad414b13e8def81bd
Cooper Goeke <cgoeke@redventures.com>
2020-07-10 15:37:39 -0400

    Project 6 -- slideshow

---
+++ project_6/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Slideshow</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+
+  <body>
+
+    <!-- HTML HERE -->
+    Project 6 (Slideshow)
+
+    <script src="slideshow.js"></script>
+  </body>
+</html>
