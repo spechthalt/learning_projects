a1432c79420da09d2a0338654996ac711cab094b
Stephanie Pechthalt <Steph@Stephanies-MacBook-Pro.local>
2020-08-26 10:28:24 -0400

    progress on p6

--- project_6/index.php
+++ project_6/index.php
@@ -9,9 +9,42 @@
   </head>
 
   <body>
+  <div class="slideshow-container">
 
-    <!-- HTML HERE -->
-    Project 6 (Slideshow)
+    <div class="slide active" style="background-image: url('assets/slide-background-1.jpg');">
+      <div class="slide-inner-wrapper">  
+          <div class="text">
+            <h2>IMAGE TITLE</h2>
+            <p>Image description</p>
+          </div>
+      </div>  
+    </div>
+
+    <div class="slide" style="background-image: url('assets/slide-background-2.jpg');">
+      <div class="slide-inner-wrapper"> 
+        <div class="text">
+          <h2>IMAGE TITLE</h2>
+          <p>Image description</p>
+        </div>
+      </div> 
+    </div>
+
+    <div class="slide" style="background-image: url('assets/slide-background-3.jpg');">
+      <div class="slide-inner-wrapper"> 
+        <div class="text">
+          <h2>IMAGE TITLE</h2>
+          <p>Image description</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="dot-selector">
+      <span class="dot active" onclick="currentSlide(1)"></span>
+      <span class="dot" onclick="currentSlide(2)"></span>
+      <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
+  </div>
 
     <script src="slideshow.js"></script>
   </body>
